f9543b722046d00b999a3462485c9ca9c1adc6a9
Cataline Baert <snuf_kin@yahoo.com>
2020-10-08 21:09:26 +0200

    refactor onclick method and state

--- src/Lottery.js
+++ src/Lottery.js
@@ -7,23 +7,26 @@ class Lottery extends Component {
   constructor(props){
     super(props);
     this.state = {
-      nums: [28, 30, 16, 22, 34, 33]
+      nums: Array.from({length: this.props.numBalls})
     }
-    this.createBallsArray = this.createBallsArray.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   static defaultProps = {
     title: 'Lotto',
     numBalls: 6,
     maxNum: 40
   }
-  createBallsArray(){
-    const {numBalls, maxNum} = this.props;
-    let newNums = [];
-    for (let i = 0; i < numBalls; i++) {
-      let randNum = Math.floor(Math.random() * maxNum + 1);
-      newNums[i] = randNum;
-    }
-    this.setState({nums: newNums})
+  generate(){
+    const {maxNum} = this.props;
+    this.setState(curState => ({
+      nums: curState.nums.map(
+        n => Math.floor(Math.random() * maxNum) + 1
+        )
+      })
+    );
+  }
+  handleClick(){
+    this.generate();
   }
   render() {
     const {nums} = this.state;
@@ -33,10 +36,10 @@ class Lottery extends Component {
         <h1>{title}</h1>
         <div className="Lottery-balls">
           {nums.map(num =>
-            <LotteryBall key={nums.indexOf(num)} num={num} />
+            <LotteryBall num={num} />
           )}
         </div>
-        <button onClick={this.createBallsArray}>Generate</button>
+        <button onClick={this.handleClick}>Generate</button>
       </div>
     );
   }
